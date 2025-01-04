ec5a1dcf2d57ad0e719dd7a9f12decbf703d0720
kettei-sproutty <kettei-sproutty@users.noreply.github.com>
2025-01-04 15:33:46 +0000

    deploy: 14af8b3140b72152c7b60a3d2228745f6cc273d7

---
+++ assets/boss-rush-2025-jam_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const main: (a: number, b: number) => number;
+export const __externref_table_alloc: () => number;
+export const __wbindgen_export_1: WebAssembly.Table;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_export_6: WebAssembly.Table;
+export const _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hca7f672167a9cc7b: (a: number, b: number, c: number) => void;
+export const closure2632_externref_shim: (a: number, b: number, c: any, d: any) => void;
+export const closure2625_externref_shim: (a: number, b: number, c: any) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hb8377a16363019d6: (a: number, b: number) => void;
+export const _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hf53c7f1bf191a048: (a: number, b: number) => void;
+export const closure98805_externref_shim: (a: number, b: number, c: any) => void;
+export const __wbindgen_start: () => void;
