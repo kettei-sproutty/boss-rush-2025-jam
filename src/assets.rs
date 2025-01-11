62cfe4eafdad075b30638834b8b9eac21a83874d
Alessio Marchi <65831826+kettei-sproutty@users.noreply.github.com>
2025-01-11 09:52:50 +0100

    feat(ui): add cursors

--- src/assets.rs
+++ src/assets.rs
@@ -1,4 +1,5 @@
 use crate::prelude::*;
+use bevy::winit::cursor::{CursorIcon, CustomCursor};
 use iyes_progress::prelude::*;
 
 #[derive(Resource)]
@@ -11,6 +12,7 @@ pub struct ExampleAssets {
 #[reflect(Resource)]
 pub struct UiAssets {
   pub font: Handle<Font>,
+  pub cursors: Vec<Handle<Image>>,
 }
 
 pub struct AssetsLoadingPlugin;
@@ -29,6 +31,10 @@ impl Plugin for AssetsLoadingPlugin {
       .add_systems(
         OnEnter(AppState::AssetsLoading),
         (load_example_assets, load_ui_assets),
+      )
+      .add_systems(
+        OnExit(AppState::AssetsLoading),
+        add_cursor,
       );
   }
 }
@@ -40,6 +46,7 @@ fn load_example_assets(
 ) {
   let tree: Handle<Image> = asset_server.load("tree.png");
   let player: Handle<Image> = asset_server.load("placeholder_char.png");
+
   loading.add(&tree);
   loading.add(&player);
 
@@ -52,7 +59,33 @@ fn load_ui_assets(
   mut loading: ResMut<AssetsLoading<AppState>>,
 ) {
   let font: Handle<Font> = asset_server.load("fonts/PixelifySans.ttf");
-  loading.add(&font);
+  let cursor_1: Handle<Image> = asset_server.load("ui/cursor1.png");
+  let cursor_2: Handle<Image> = asset_server.load("ui/cursor2.png");
+  let cursor_3: Handle<Image> = asset_server.load("ui/cursor3.png");
+  let cursor_4: Handle<Image> = asset_server.load("ui/cursor4.png");
 
-  commands.insert_resource(UiAssets { font });
+  loading.add(&font);
+  loading.add(&cursor_1);
+  loading.add(&cursor_2);
+  loading.add(&cursor_3);
+  loading.add(&cursor_4);
+
+  commands.insert_resource(UiAssets {
+    font,
+    cursors: vec![cursor_1, cursor_2, cursor_3, cursor_4],
+  });
+}
+
+fn add_cursor(
+  mut commands: Commands,
+  window: Single<Entity, With<Window>>,
+  ui_assets: Res<UiAssets>,
+) {
+  commands.entity(*window).insert(
+    CursorIcon::Custom(CustomCursor::Image {
+      handle: ui_assets.cursors[0].clone(),
+      hotspot: (0, 0),
+    })
+    .clone(),
+  );
 }
